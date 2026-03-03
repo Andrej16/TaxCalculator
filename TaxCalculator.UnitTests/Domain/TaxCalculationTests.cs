7395b9e5e7a04cc295c8a33915cb80336f14c00e
Andrii Shylin1 <ashylin@adnoc.ae>
2026-03-03 15:40:10 +0200

    Added tests

--- TaxCalculator.UnitTests/Domain/TaxCalculationTests.cs
+++ TaxCalculator.UnitTests/Domain/TaxCalculationTests.cs
@@ -1,4 +1,6 @@
-﻿using TaxCalculator.Domain.Entities;
+﻿using System.Globalization;
+using System.Reflection;
+using TaxCalculator.Domain.Entities;
 
 namespace TaxCalculator.UnitTests.Domain;
 
@@ -25,5 +27,54 @@ public sealed class TaxCalculationTests
 
         // Assert
         Assert.IsNotNull(ex, "Expected ArgumentException to be thrown when bands is empty.");
+    }
+
+    [TestMethod]
+    public void Calculate_ValidBands_ReturnsCorrectTaxCalculation()
+    {
+        // Arrange
+        var grossAnnualSalary = 10000d;
+        var expectedAnnualTaxPaid = 1000d;
+        
+        var bands = new List<TaxBandType>
+        {
+            CreateTaxBandType("Band 1", 0, 5000, 0),
+            CreateTaxBandType("Band 2", 5000, 20000, 20),
+            CreateTaxBandType("Band 3", 20000, int.MaxValue, 40)
+        };
+
+        // Act
+        var result = TaxCalculation.Calculate(grossAnnualSalary, bands);
+
+        // Assert
+        Assert.AreEqual(grossAnnualSalary, result.GrossAnnualSalary);
+        Assert.AreEqual(grossAnnualSalary / 12, result.GrossMonthlySalary);
+        Assert.AreEqual(grossAnnualSalary - expectedAnnualTaxPaid, result.NetAnnualSalary);
+        Assert.AreEqual(result.NetAnnualSalary / 12, result.NetMonthlySalary);
+        Assert.AreEqual(expectedAnnualTaxPaid, result.AnnualTaxPaid);
+        Assert.AreEqual(expectedAnnualTaxPaid / 12, result.MonthlyTaxPaid);
+    }
+
+    private static TaxBandType CreateTaxBandType(string name, int minRange, int maxRange, int taxRate)
+    {
+        Type objectType = typeof(TaxBandType);
+        var flags = BindingFlags.Instance | BindingFlags.NonPublic;
+
+        if (Activator.CreateInstance(
+            objectType,
+            flags,
+            null,
+            null,
+            CultureInfo.CurrentCulture) is not TaxBandType band)
+        {
+            throw new InvalidOperationException("Failed to create instance of TaxBandType.");
+        }
+
+        objectType.GetProperty(nameof(TaxBandType.Name))!.SetValue(band, name);
+        objectType.GetProperty(nameof(TaxBandType.MinRange))!.SetValue(band, minRange);
+        objectType.GetProperty(nameof(TaxBandType.MaxRange))!.SetValue(band, maxRange);
+        objectType.GetProperty(nameof(TaxBandType.TaxRate))!.SetValue(band, taxRate);
+        
+        return band;
     }
 }
